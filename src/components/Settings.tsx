7a3644fbb129a28f97bd15f180f7a22d3bfe8a78
Praise Kayode <kpraisekolawole@gmail.com>
2024-01-24 12:07:38 +0100

    Made some updates to the Settings page

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -26,27 +26,34 @@ const Settings: React.FC = () => {
 
 
             <Container className='settings min-vh-100' fluid>
-                <Row className='mt-3'>
-                    <Breadcrumb>
-                        <Breadcrumb.Item href="#">Dashboard</Breadcrumb.Item>
-                        <Breadcrumb.Item active>Update Password</Breadcrumb.Item>
-                    </Breadcrumb>
+                <Row className="h-100 pt-3 justify-content-center align-items-center">
+                    <Col lg="9">
+                        <Breadcrumb>
+                            <Breadcrumb.Item href="#">Dashboard</Breadcrumb.Item>
+                            <Breadcrumb.Item active>Update Password</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Col>
                 </Row>
 
+                <Row className="h-100 pt-3 justify-content-center align-items-center">
+                    <Col lg="9">
                 <h4 id='settings-header'>Settings - Update Password</h4>
+                    </Col>
 
+                </Row >
 
-                <Row>
-                    <Col>
+                <Row className="h-100 pt-2 justify-content-center align-items-center">
+                    <Col lg="9">
 
                         <Form className="bg-white p-4 mt-3">
-                            <h5 className='text-center'> To update your password </h5>
+                            <h5 className='text-center'><b> To update your password </b></h5>
 
                             <p className='text-center small'>
-                                Enter your new password in the input field below and click the 'Update Password' button to update your password
+                                    Enter your new password in the input field below and 
+                                    click the 'Update Password' button to update your password 
                             </p>
 
-                            <Form.Label>Password*</Form.Label>
+                            <Form.Label> <b>Password*</b></Form.Label>
                             <Row>
                                 <Col>
                                     <InputGroup className="mb-3">
