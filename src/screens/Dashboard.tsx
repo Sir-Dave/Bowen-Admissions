903f5d7c5b8ff0be9899f2dc4b6df226319e3697
Abiola David <davidolanrewaju2000@gmail.com>
2024-02-20 08:03:11 +0100

    Moved admission components into dashboard file

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -6,9 +6,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBuilding, faListAlt, faCreditCard, faBell } from '@fortawesome/free-regular-svg-icons';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import Header from '../components/Header';
-import EmptyAdmissionCard from '../components/EmptyAdmissionCard';
-import CurrentAdmissionCard from '../components/CurrentAdmissionCard';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
   var hasAdmission = true;
@@ -137,4 +137,88 @@ const Dashboard: React.FC = () => {
   );
 }
 
+const EmptyAdmissionCard: React.FC = () => {
+  return (
+      <Card>
+          <Card.Header>Current Admission and Application</Card.Header>
+
+          <Card.Body className='d-flex flex-column align-items-center'>
+              <Button variant="primary" className='btn w-100'>Current Admission</Button>
+
+              <Row className='w-100 mt-3'>
+                  <div className="d-flex justify-content-between align-items-center">
+                      <p className='text-dates'> <strong>Starts</strong>  </p>
+                      <p className='text-dates'> Wednesday, May 3, 2023 </p>
+                  </div>
+
+              </Row>
+
+
+              <Row className='w-100'>
+                  <div className="d-flex justify-content-between align-items-center">
+                      <p className='text-dates'> <strong>Ends</strong>  </p>
+                      <p className='text-dates'> Wednesday, January 31, 2024</p>
+                  </div>
+
+              </Row>
+
+              <Button variant="primary" className='btn w-100'>Current Application</Button>
+
+              <Card.Text className='mt-3'><strong>No Application Currently</strong></Card.Text>
+
+              <Button variant="outline-success" className='btn w-100'>View Form History</Button>
+          </Card.Body>
+      </Card>
+
+  );
+}
+
+const CurrentAdmissionCard: React.FC = () => {
+  const navigate = useNavigate();
+
+  const viewFormSummary = () => {
+      navigate('/applicant/form/application');
+  };
+  return (
+      <Card>
+          <Card.Header>Current Admission and Application</Card.Header>
+
+          <Card.Body className='d-flex flex-column align-items-center'>
+              <Button variant="primary" className='btn w-100'>Current Admission</Button>
+
+              <Row className='w-100 mt-3'>
+                  <div className="d-flex justify-content-between align-items-center">
+                      <p className='text-dates'> <strong>Starts</strong>  </p>
+                      <p className='text-dates'> Wednesday, May 3, 2023 </p>
+                  </div>
+
+              </Row>
+
+
+              <Row className='w-100'>
+                  <div className="d-flex justify-content-between align-items-center">
+                      <p className='text-dates'> <strong>Ends</strong>  </p>
+                      <p className='text-dates'> Wednesday, January 31, 2024</p>
+                  </div>
+
+              </Row>
+
+              <Button variant="primary" className='btn w-100'>Current Application</Button>
+
+              <Card.Text className='mt-3'>Application Form Submitted</Card.Text>
+
+              <Button variant="outline-success" className='btn w-100' onClick={viewFormSummary}>Print Form Summary </Button>
+
+              <Button variant="outline-info" className='mt-4 btn w-100'>Print Letter of Admission</Button>
+              <Button variant="outline-info" className='mt-2 btn w-100'>Pay Acceptance Fee</Button>
+              <Button variant="outline-success" className='mt-2 btn w-100'>Print Affidavit</Button>
+              <Button variant="outline-success" className='mt-2 btn w-100'>Pay School Fees</Button>
+              <Button variant="outline-success" className='mt-2 btn w-100'>Upload Documents</Button>
+              <Button variant="outline-success" className='mt-2 btn w-100'>Fill ID Card Information</Button>
+              <Button variant="outline-info" className='mt-2 btn w-100'>Download Pledge Form</Button>
+          </Card.Body>
+      </Card>
+
+  );
+}
 export default Dashboard;
