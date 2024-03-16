c09849700664cc9086c3214fdb95a709919782f5
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-16 08:45:04 +0100

    Use provider in application

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,37 @@ import StaffLogin from './screens/auth/StaffLogin';
 import EmailConfirm from './screens/auth/EmailConfirm';
 import Application from './screens/applicant/Application';
 import RegisterAccount from './screens/auth/RegisterAccount';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 
 import './App.css';
 import StaffDashboard from './screens/staff/StaffDashboard';
+import { UserProvider } from './context/AuthContext';
 
 function App() {
   return (
+
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={< Dashboard />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/referral-login" element={<ReferralLogin />} />
-        <Route path="/staff-login" element={<StaffLogin />} />
-        <Route path="/confirm-email" element={<EmailConfirm />} />
-        <Route path="/register-account" element={<RegisterAccount />} />
-        <Route path="/applicant/form/application" element={<Application/>} />
-        <Route path="/admin/management-console/dashboard" element={<StaffDashboard/>} />
-        
-      </Routes>
+      <ToastContainer />
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={< Dashboard />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/referral-login" element={<ReferralLogin />} />
+          <Route path="/staff-login" element={<StaffLogin />} />
+          <Route path="/confirm-email" element={<EmailConfirm />} />
+          <Route path="/register-account" element={<RegisterAccount />} />
+          <Route path="/applicant/form/application" element={<Application />} />
+          <Route path="/admin/management-console/dashboard" element={<StaffDashboard />} />
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
+
+
   );
 }
 
