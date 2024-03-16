924eaa4106cd03c52a671cdeec0d4f6db370d0d4
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-16 08:38:03 +0100

    Reafactored dtos to separate package

---
+++ src/dtos/user/UserDto.ts
@@ -0,0 +1,19 @@
+interface UserProfile {
+    id: number,
+    surname: string,
+    firstName: string
+    middleName: string | null
+    phoneNo: string
+    dateOfBirth: string
+    nationality: string
+    stateOfOrigin: string
+    localGovernment: string
+    religion: string
+    denomination: string
+    gender: string
+    maritalStatus: string
+    maidenName: string | null
+    email: string
+    countryCode: string
+    userType: number
+}
